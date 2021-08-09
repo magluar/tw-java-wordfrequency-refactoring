4f8a15bb6a590335e977280c42f1642ab5d788f4
magluar <aryann.magluyan@oocl.com>
2021-08-09 15:49:13 +0800

    [WordFrequency][magluar] extract method to create getWordInfos

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,24 +13,7 @@ public class WordFrequencyGame {
             return sentence + " " + ONE;
         } else {
             try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(BLANK_SPACE);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, ONE);
-                    wordInfoList.add(wordInfo);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-                List<WordInfo> wordMapList = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    wordMapList.add(wordInfo);
-                }
-                wordInfoList = wordMapList;
+                List<WordInfo> wordInfoList = getWordInfos(sentence);
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
@@ -43,6 +26,28 @@ public class WordFrequencyGame {
                 return "Calculate Error";
             }
         }
+    }
+
+    private List<WordInfo> getWordInfos(String sentence) {
+        //split the input string with 1 to n pieces of spaces
+        String[] words = sentence.split(BLANK_SPACE);
+
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordInfo wordInfo = new WordInfo(word, ONE);
+            wordInfoList.add(wordInfo);
+        }
+
+        //get the map for the next step of sizing the same word
+        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
+
+        List<WordInfo> wordMapList = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            wordMapList.add(wordInfo);
+        }
+        wordInfoList = wordMapList;
+        return wordInfoList;
     }
 
 
