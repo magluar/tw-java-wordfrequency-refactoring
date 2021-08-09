2834ae5f517557617b83b411762987be680968f7
magluar <aryann.magluyan@oocl.com>
2021-08-09 18:32:48 +0800

    [WordFrequency][magluar] refactor: remove unused getListMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -38,20 +38,4 @@ public class WordFrequencyGame {
     private List<String> getWords(String sentence) {
         return Arrays.asList(sentence.split(BLANK_SPACE));
     }
-
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-        return map;
-    }
 }
