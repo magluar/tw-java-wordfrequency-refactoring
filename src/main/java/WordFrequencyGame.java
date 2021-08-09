7e096aa0aaae54a42a620d04d31458306b5030b6
magluar <aryann.magluyan@oocl.com>
2021-08-09 18:21:52 +0800

    [WordFrequency][magluar] refactor: delete if else statement

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,37 +1,28 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class WordFrequencyGame {
     public static final String BLANK_SPACE = "\\s+";
     public static final Integer ONE = 1;
 
     public String getResult(String sentence){
-        if (sentence.split(BLANK_SPACE).length==1) {
-            return sentence + " " + ONE;
-        } else {
-            try {
-                List<WordInfo> wordInfoList = getWordInfos(sentence);
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        try {
+            List<WordInfo> wordInfoList = getWordInfos(sentence);
+            wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo word : wordInfoList) {
-                    String resultString = word.getValue() + " " +word.getWordCount();
-                    joiner.add(resultString);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
+            StringJoiner joiner = new StringJoiner("\n");
+            for (WordInfo word : wordInfoList) {
+                String resultString = word.getValue() + " " +word.getWordCount();
+                joiner.add(resultString);
             }
+            return joiner.toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
     }
 
     private List<WordInfo> getWordInfos(String sentence) {
         //split the input string with 1 to n pieces of spaces
         String[] words = sentence.split(BLANK_SPACE);
-
         List<WordInfo> wordInfoList = new ArrayList<>();
         for (String word : words) {
             WordInfo wordInfo = new WordInfo(word, ONE);
